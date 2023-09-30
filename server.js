934f61f88acbef33752c4de3646d919363a61633
Zakariyah Ali <99949368+alibaba0010@users.noreply.github.com>
2023-09-30 10:20:54 +0100

    Update server.js

--- server.js
+++ server.js
@@ -25,9 +25,24 @@ const upload = multer({
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(json()).use("/uploads", async (req, res) => {
-  res.sendFile(express.static((__dirname, "index.html")));
+
+// Handle video upload
+app.post('/upload', (req, res) => {
+    upload(req, res, (err) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Error uploading file.');
+        } else {
+            const videoPath = req.file.path;
+            // You can save the videoPath to a database if needed
+            // For now, just send the path to the client for playing the video
+            res.send(<video width="640" height="360" controls><source src="${videoPath}" type="video/mp4">Your browser does not support the video tag.</video>);
+        }
+    });
 });
+
+app.use(json()).use("/uploads", express.static((__dirname, "uploads"));
+
 const PORT = process.env.PORT || 2001;
 (async () => {
   app.listen(PORT, () =>
